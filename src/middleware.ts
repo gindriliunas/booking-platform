0b0af78c044b3483e11a7a0a3517a41d0aa011a3
Gintas <g.indriliunas@gmail.com>
2026-03-20 16:38:01 +0000

    sign up fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,7 @@ const isPublicRoute = createRouteMatcher([
   "/api/stripe/webhook(.*)",
 ]);
 
-export const proxy = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
